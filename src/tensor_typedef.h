f008338bba330cc28c53e7c0760d13ea738de8f1
Yongjoo Ahn <yongjoo1.ahn@samsung.com>
2025-01-22 11:18:42 +0900

    Remove license conflicts in those two imported header files
    Signed-off-by: Yongjoo Ahn <yongjoo1.ahn@samsung.com>

--- src/tensor_typedef.h
+++ src/tensor_typedef.h
@@ -1,19 +1,4 @@
 /**
- * NNStreamer Common Header, Typedef part, for export as devel package.
- * Copyright (C) 2018 MyungJoo Ham <myungjoo.ham@samsung.com>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation;
- * version 2.1 of the License.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- */
-/**
  * @file	tensor_typedef.h
  * @date	01 Jun 2018
  * @brief	Common header file for NNStreamer, the GStreamer plugin for neural networks
